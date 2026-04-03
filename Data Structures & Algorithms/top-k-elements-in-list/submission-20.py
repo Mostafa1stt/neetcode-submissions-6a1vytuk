165e38a92235b52a6a2e0716fc4689ed9bd828b9
mostafa abdelkareem <130945329+Mostafa1stt@users.noreply.github.com>
2026-04-03 20:39:39 +0200

    Add: top-k-elements-in-list - submission-20

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-20.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mapl = dict()
+        for i in nums:
+            if i in mapl:
+                mapl[i] += 1
+            else:
+                mapl[i] = 0
+        keys_only = sorted(mapl, key=lambda x: mapl[x],reverse=True)
+        return keys_only[:k]
